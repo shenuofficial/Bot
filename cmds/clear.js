f355d2555b97643ae49a4302ad81b763060b95e7
shenuofficial <41961180+shenuofficial@users.noreply.github.com>
2018-08-02 15:13:41 +0300

    Update clear.js

--- cmds/clear.js
+++ cmds/clear.js
@@ -1,13 +1,1 @@
-const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
-	if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("❌ You do not have access to this command");
-	if(!args[0]) return message.channel.send("off");
-	message.channel.bulkDelete(args[0]).then(() => {
-		message.channel.send(`🗑️ Cleared ${args[0]} messages.`).then(msg => msg.delete(5000));
-	});
-}
-
-module.exports.help = {
-	name: "clear"
-}
