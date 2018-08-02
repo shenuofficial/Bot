00888aed9353a1af602abe7c8d58396c78b473f6
shenuofficial <41961180+shenuofficial@users.noreply.github.com>
2018-08-02 15:14:14 +0300

    Update icon.js

--- cmds/icon.js
+++ cmds/icon.js
@@ -1,18 +1,1 @@
-module.exports.run = async (bot, message, args) => {
-	let msg = await message.channel.send("Generating server avatar...");
 
-	if(!message.guild.iconURL) return msg.edit("No server avatar");
-
-	await message.channel.send({files: [
-		{
-			attachment: message.guild.iconURL,
-			name: "server_avatar.png"
-		}
-	]});
-
-	msg.delete();
-}
-
-module.exports.help = {
-	name: "icon"
-}
